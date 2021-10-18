bf1c10c9a7b068a8cd07962803a452e8fa2862dd
m4rc3l05 <joaobat@live.com.pt>
2021-10-18 22:38:36 +0100

    Add migrations
    Signed-off-by: m4rc3l05 <joaobat@live.com.pt>

---
+++ database/migrations/20211014131526_add_software_table.js
@@ -0,0 +1,32 @@
+/**
+ * @param {import("knex").Knex} knex
+ */
+export async function up(knex) {
+  await knex.schema.withSchema("core").createTable("softwares", (table) => {
+    table.uuid("id").primary().defaultTo(knex.raw(`"core".uuid_generate_v4()`));
+    table.text("name").notNullable();
+    table.text("description").nullable();
+    table.text("lead").nullable();
+    table.text("iconPath").nullable();
+    table
+      .uuid("alternativeTo")
+      .nullable()
+      .references("id")
+      .inTable(`${"core"}.softwares`);
+    table.enum("pricing", ["free", "freemium", "paid"]).notNullable();
+    table.text("licence").nullable();
+    table.timestamp("createdAt", { useTz: true }).defaultTo(knex.fn.now());
+    table.timestamp("updatedAt", { useTz: true }).defaultTo(knex.fn.now());
+  });
+
+  await knex.raw(
+    `create trigger update_timestamp before update on "core"."softwares" for each row execute procedure "core".update_timestamp()`,
+  );
+}
+
+/**
+ * @param {import("knex").Knex} knex
+ */
+export async function down(knex) {
+  await knex.schema.withSchema("core").dropTable("softwares");
+}
