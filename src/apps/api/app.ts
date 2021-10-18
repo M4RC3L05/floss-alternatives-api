39bbfbcc356fd02aa2dc4a577afae79fa83cf9f8
m4rc3l05 <joaobat@live.com.pt>
2021-10-18 23:17:05 +0100

    Add api
    Signed-off-by: m4rc3l05 <joaobat@live.com.pt>

---
+++ src/apps/api/app.ts
@@ -0,0 +1,28 @@
+import { App } from "@tinyhttp/app";
+import helmet from "helmet";
+import { cors } from "@tinyhttp/cors";
+import config from "config";
+import { router } from "./router";
+import { errorHandlerMiddleware } from "#src/core/http/middlewares/error-handler.middleware";
+import { httpErrorMapper } from "#src/core/http/errors/http-error-mapper";
+
+export const app = () => {
+  const app = new App({
+    onError: errorHandlerMiddleware({ mappers: [httpErrorMapper] }),
+  });
+
+  if (config.get<string>("mode") === "development") {
+    app.use(async (...args) => {
+      const { logger } = await import("@tinyhttp/logger");
+
+      logger({ emoji: true })(...args);
+    });
+  }
+
+  app.use(helmet());
+  app.use(cors());
+
+  router(app);
+
+  return app;
+};
