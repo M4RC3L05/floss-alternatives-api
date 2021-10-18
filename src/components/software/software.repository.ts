ccf0408192afe06d60f530b35b3bc107c214b4b5
m4rc3l05 <joaobat@live.com.pt>
2021-10-18 23:14:07 +0100

    Add software repo
    Signed-off-by: m4rc3l05 <joaobat@live.com.pt>

---
+++ src/components/software/software.repository.ts
@@ -0,0 +1,4 @@
+import { SoftwareModel } from "./software.model";
+import { AbstractRepository } from "#src/core/database/abstract-repository";
+
+export class SoftwareRepository extends AbstractRepository<SoftwareModel> {}
